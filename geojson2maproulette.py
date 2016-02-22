b8321efa1cea1dde7e7aea7090e87eae5633a997
Martijn van Exel <m@rtijn.org>
2016-02-22 11:13:38 -0700

    initial commit

---
+++ geojson2maproulette.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import geojson
+import maproulette
+import docopt
+
+if __name__ == "__main__":
+	# parse arguments
+	# parse geojson to task objects
+	# post to MapRoulette
